3d2273b1a9a6f35f1cb81ab7034403b45de79901
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-02-28 15:32:58 +0100

    renderTo was lost and chart could not render
    Since dropping jQuery depedency in favor of angular.extend() it was not
    possible to render a chart without specifying manually the renderTo
    property. This was due to the fact that angular.extend() is not recursive
    (whereas jQuery was).
    
    To avoid adding depedency we merge manually the few properties.

--- src/directives/highchart.js
+++ src/directives/highchart.js
@@ -14,26 +14,20 @@ angular.module('chartsExample.directives',[])
     replace: true,
 
     link: function (scope, element, attrs) {
-      var chartsDefaults = {
-        chart: {
-          renderTo: element[0],
-          type: attrs.type || null,
-          height: attrs.height || null,
-          width: attrs.width || null
-        }
-      };
-      
+
         //Update when charts data changes
         scope.$watch(function() { return scope.chartData; }, function(value) {
           if(!value) return;
-            // We need deep copy in order to NOT override original chart object.
-            // This allows us to override chart data member and still the keep
-            // our original renderTo will be the same
-            var newSettings = {};
-            angular.extend(newSettings, chartsDefaults, scope.chartData);
-            scope.chartObj = new Highcharts.Chart(newSettings);
+
+            // use default values if nothing is specified in the given settings
+            scope.chartData.chart.renderTo = scope.chartData.chart.renderTo || element[0];
+            if (attrs.type) scope.chartData.chart.type = scope.chartData.chart.type || attrs.type;
+            if (attrs.height) scope.chartData.chart.height = scope.chartData.chart.height || attrs.height;
+            if (attrs.width) scope.chartData.chart.width = scope.chartData.chart.type || attrs.width;
+
+            scope.chartObj = new Highcharts.Chart(scope.chartData);
         });
       }
-    }
+    };
 
 });
